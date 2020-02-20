a2ba666034d1a8fe820e7604f018512af84a5b8f
Mélissa Leal <sebriniel@hotmail.fr>
2020-02-20 17:42:31 +0100

    LocationAmountByEmailReducer

---
+++ src/LocationAmountByEmailReducer.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class LocationAmountByEmailReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    protected void reduce(Text email, Iterable<IntWritable> locationAmountList, Reducer<Text, IntWritable, Text, IntWritable>.Context context)
+        throws IOException, InterruptedException
+    {
+        long locationAmountSum = 0;
+        long locationAmountNum = 0;
+        
+        for(IntWritable locationAmount : locationAmountList) {
+            locationAmountSum += locationAmount.get();
+            locationAmountNum ++;
+        }
+        context.write(email, new IntWritable((int)(locationAmountSum/locationAmountNum)));
+    }
+}
