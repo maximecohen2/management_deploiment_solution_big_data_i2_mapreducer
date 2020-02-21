5115d2fb91dd7ea4c532af5f4820796ade3e96c5
Maxime Cohen <maxime.cohen2@epsi.fr>
2020-02-21 09:20:23 +0100

    + add Mapper AmountRentPerCustomer

---
+++ src/AmountRentPerCustomerMapper.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+
+public class AmountRentPerCustomerMapper extends Mapper<LongWritable, Text, Text, FloatWritable> {
+	@Override
+	protected void map(LongWritable linenumber, Text line, Mapper<LongWritable, Text, Text, FloatWritable>.Context context) 
+			throws IOException, InterruptedException {
+		String[] fields = line.toString().split(";");
+		String email = fields[5].split(" ")[0];
+		try {
+			// int age = Integer.parseInt(fields[1]);
+			// context.write(new Text(names[0]), new IntWritable(age));
+			float amount = Float.parseFloat(fields[2]);
+			context.write(new Text(email), new FloatWritable(amount));
+		} catch (NumberFormatException e) {
+			context.getCounter("Error", e.getMessage()).increment(1);
+		}
+		
+	}
+}
