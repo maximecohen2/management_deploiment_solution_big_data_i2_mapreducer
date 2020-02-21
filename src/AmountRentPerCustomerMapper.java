29c65eba93698677e871c70bc97d7d7516a35bcb
Maxime Cohen <maxime.cohen2@epsi.fr>
2020-02-21 09:26:35 +0100

    + fix parser

--- src/AmountRentPerCustomerMapper.java
+++ src/AmountRentPerCustomerMapper.java
@@ -11,7 +11,7 @@ public class AmountRentPerCustomerMapper extends Mapper<LongWritable, Text, Text
 	protected void map(LongWritable linenumber, Text line, Mapper<LongWritable, Text, Text, FloatWritable>.Context context) 
 			throws IOException, InterruptedException {
 		String[] fields = line.toString().split(";");
-		String email = fields[5].split(" ")[0];
+		String email = fields[5].replaceAll("\"", "").toString();
 		try {
 			// int age = Integer.parseInt(fields[1]);
 			// context.write(new Text(names[0]), new IntWritable(age));
